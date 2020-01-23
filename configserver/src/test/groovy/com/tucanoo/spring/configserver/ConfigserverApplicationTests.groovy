f09fc913211b69fdd3b6bb1d3f31481a3768d247
davebrown1975 <dave@tucanoo.com>
2020-01-23 13:20:15 +0100

    setup with spring cloud config server, to dictate ports for the individual services.

---
+++ configserver/src/test/groovy/com/tucanoo/spring/configserver/ConfigserverApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.tucanoo.spring.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
